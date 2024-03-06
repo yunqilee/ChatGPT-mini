bebb25b0f6e379f4677b075055cb329ad78627bb
yunqilee <yli380479@gmail.com>
2024-03-05 23:51:17 -0600

    Modification - Mar 5

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, render_template, request, jsonify
 
 import os
@@ -11,27 +12,28 @@ def index():  # put application's code here
     return render_template("my-chatgpt.html")
 
 
-@app.route('/generate_text', methods=["POST"])
+@app.route('/generate_text', methods=["POST", "GET"])
 def generate_text():
-    prompt = request.form["prompt"]
+    prompt = request.args.get("prompt", "")
     prompt = str(prompt).strip()
+    if prompt:
+        def stream():
+            client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+            completion = client.chat.completions.create(
+                model='gpt-3.5-turbo',
+                messages=[
+                    {'role': 'user', 'content': prompt}
+                ],
+                stream=True
+            )
+            for chunk in completion:
+                if chunk.choices[0].finish_reason is not None:
+                    data = "[DONE]"
+                else:
+                    data = chunk.choices[0].delta.content
+                yield "data: %s\n\n" % data.replace("\n", "<br>")
 
-    if not prompt:
-        return "No prompt provided"
-
-    try:
-        client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-        completion = client.chat.completions.create(
-            model='gpt-3.5-turbo',
-            messages=[
-                {'role': 'user', 'content': prompt}
-            ]
-        )
-    except Exception as e:
-        return "Something went wrong while accessing OpenAI API"
-
-    return completion.choices[0].message.content
-
+        return flask.Response(stream(), mimetype="text/event-stream")
 
 if __name__ == '__main__':
     app.run()
